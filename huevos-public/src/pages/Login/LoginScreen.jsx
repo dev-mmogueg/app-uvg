2a10b114ba18d2c10a448b50328e137a9a1b4764
unknown <mmogue-2021220@kinal.edu.gt>
2024-04-28 22:42:12 -0600

    Se agrego front de registro, asi como prototipo de logo

--- huevos-public/src/pages/Login/LoginScreen.jsx
+++ huevos-public/src/pages/Login/LoginScreen.jsx
@@ -8,6 +8,7 @@ import axios from 'axios'
 
 /* ---- IMPORT's ---- */
 import './LoginStyle.css';
+import isotipo from '../../assets/imgs/logo-app-uvg-3.png'
 
 export const LoginScreen = () => {
 
@@ -17,10 +18,20 @@ export const LoginScreen = () => {
 
   /* ---- STATE's ---- */
   const [open_reg, setOpen_reg] = useState(false);
+  const [curr, setCurr] = useState(0)
   const [form, setForm] = useState({
     usernameOrEmail: '',
     password: '',
   });
+
+  /* ---- FUCTIONS ---- */
+  const moveView = () => {
+    setCurr(curr => curr === 2 ? 0 : curr + 1);
+  }
+
+  const moveBack = () => {
+    setCurr(curr => curr !== 0 ? curr - 1 : 0);
+  }
 
   const handleChange = (e) => {
     setForm({
@@ -47,7 +58,23 @@ export const LoginScreen = () => {
         showConfirmButton: false,
         showCloseButton: false,
         timer: 2000
-      })
+      });
+    }
+  }
+
+  const register = async () => {
+    try {
+      e.preventDefault();
+    } catch (err) {
+      console.error(err);
+      Swal.fire({
+        title: err.response.data.message,
+        icon: 'info',
+        iconColor: 'orange',
+        showConfirmButton: false,
+        showCloseButton: false,
+        timer: 2000
+      });
     }
   }
 
@@ -55,9 +82,16 @@ export const LoginScreen = () => {
     <section
       className={`min-h-screen gradient-bg-animation flex justify-center items-center relative overflow-hidden`}
     >
-      <div className='max-md:w-full p-8 flex flex-col justify-around gap-9'>
+      <div className='max-md:w-full p-6 flex flex-col justify-around gap-6'>
         <div className='w-full'>
-          <h1 className='w-full text-center pb-8 text-white font-bold text-4xl'>Log In to Raven</h1>
+          <div className='flex justify-center items-center'>
+            <img
+              className={`w-64`}
+              src={isotipo}
+              alt="isotipo"
+            />
+          </div>
+          {/* <h1 className='w-full text-center pb-8 text-white font-bold text-4xl'>Log In to Raven</h1> */}
           <div className='flex flex-col gap-3'>
             <button className='w-full px-4 py-2 border-[1px] flex justify-center gap-6 items-center rounded-3xl border-white'>
               <span
@@ -144,15 +178,125 @@ export const LoginScreen = () => {
       >
         <div
           className={`relative p-3`}
-          onClick={() => { setOpen_reg(false) }}
         >
-          <button className={`absolute rotate-45 right-4 top-4 ${open_reg ? 'opacity-100' : 'opacity-0 hidden'}`}>
+          <button
+            onClick={() => { setOpen_reg(false), setTimeout(() => setCurr(0), 500) }}
+            className={`absolute z-[2] rotate-45 right-4 top-4 ${open_reg ? 'opacity-100' : 'opacity-0 hidden'}`}
+          >
             <GoPlus size={32} />
           </button>
           <div
-            className={``}
+            id={`container-register`}
+            style={{
+              transform: `translateX(-${curr * (screen.width)}px)`
+            }}
+            className={`relative transition-all duration-500 delay-200 flex gap-6`}
           >
-
+            <div
+              className={`min-w-full min-h-[70vh] mt-16 p-3`}
+            >
+              <h1
+                className={`w-full text-center font-semibold text-4xl text-lime-700 border-b-2 pb-2 border-lime-700`}
+              >
+                REGISTRO
+              </h1>
+              <div className={`flex flex-col justify-around items-center space-y-3`}>
+                <img
+                  className={`w-72`}
+                  src="https://3dicons.sgp1.cdn.digitaloceanspaces.com/v1/dynamic/color/boy-dynamic-color.png"
+                />
+                <div className='w-full'>
+                  <label
+                    htmlFor="name"
+                    className='text-base text-lime-600 font-semibold'
+                  >
+                    Nombre
+                  </label>
+                  <input
+                    className='w-full px-3 py-2 mt-1 border-[2px] border-lime-600 bg-lime-100 rounded-lg text-lime-800'
+                    placeholder='Nombre (Ejemplo, Juan Emilio)'
+                    onChange={handleChange}
+                    name='name'
+                    inputMode='text'
+                  />
+                </div>
+                <div className='w-full'>
+                  <label
+                    htmlFor="surname"
+                    className='text-base text-lime-600 font-semibold'
+                  >
+                    Apellido
+                  </label>
+                  <input
+                    className='w-full px-3 py-2 mt-1 border-[2px] border-lime-600 bg-lime-100 rounded-lg text-lime-800'
+                    placeholder='Apellido (Ejemplo, Garcia Espinoza)'
+                    onChange={handleChange}
+                    name='surname'
+                    inputMode='text'
+                  />
+                </div>
+                <div className={`w-full flex justify-around gap-3 pt-4`}>
+                  <button
+                    className={`bg-lime-800 w-full px-4 py-2 rounded-2xl font-semibold text-yellow-50`}
+                  >
+                    Cerrar
+                  </button>
+                  <button
+                    onClick={(e) => { e.preventDefault(), moveView() }}
+                    className={`bg-lime-400 w-full px-4 py-2 rounded-2xl font-semibold text-yellow-50`}
+                  >
+                    Siguiente
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div
+              className={`min-w-full min-h-[70vh] mt-16 p-3`}
+            >
+              <h1
+                className={`w-full text-center font-semibold text-4xl text-lime-700 border-b-2 pb-2 border-lime-700`}
+              >
+                REGISTRO
+              </h1>
+              <div
+                className={`flex flex-col justify-around items-center space-y-3`}
+              >
+                <img
+                  className={`w-72`}
+                  src="https://3dicons.sgp1.cdn.digitaloceanspaces.com/v1/dynamic/clay/copy-dynamic-clay.png"
+                />
+                <div className='w-full'>
+                  <label
+                    htmlFor="email"
+                    className='text-base text-lime-600 font-semibold'
+                  >
+                    Correo
+                  </label>
+                  <input
+                    className='w-full px-3 py-2 mt-1 border-[2px] border-lime-600 bg-lime-100 rounded-lg text-lime-800'
+                    placeholder='ejemplo@dominio.com'
+                    onChange={handleChange}
+                    name='email'
+                    inputMode='email'
+                  />
+                </div>
+                <div className={`w-full flex justify-around gap-3 pt-4`}>
+                  <button
+                    onClick={(e) => { e.preventDefault(), moveBack() }}
+                    className={`bg-lime-800 w-full px-4 py-2 rounded-2xl font-semibold text-yellow-50`}
+                  >
+                    Cerrar
+                  </button>
+                  <button
+                    onClick={(e) => { e.preventDefault(), moveView() }}
+                    className={`bg-lime-400 w-full px-4 py-2 rounded-2xl font-semibold text-yellow-50`}
+                  >
+                    Siguiente
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className={`min-w-full min-h-[70vh] bg-blue-500 mt-16`} onClick={() => { moveView() }}></div>
           </div>
         </div>
       </div>
